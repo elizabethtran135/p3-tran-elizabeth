453f68a76517d41131eb16d1bcd568365213d410
Elizabeth Tran <elizabethtran135@gmail.com>
2019-12-10 23:09:04 -0500

    Modify Javascript, comments, shorten code

--- src/main.js
+++ src/main.js
@@ -1,32 +1,33 @@
 //Global Varibales
 
-//layer one
+//Layer One Colors
 const layerOne = document.getElementById('layer1');
 const colorBoxes = document.getElementsByClassName('colorblock');
 
-//layer Two
+//layer Two Numbers Set One
 const layerTwo = document.getElementById('layer2');
 const numberOneBoxes = document.getElementsByClassName('numberblock');
 
-//layer Three
+//layer Three Numbers Set Two
 const layerThree = document.getElementById('layer3');
 const numberTwoBoxes = document.getElementsByClassName('numberblock2');
 
-//layer four
+//Layer Four Message Reveal
 const layerFour = document.getElementById('layer4');
 
-//functionality color and shorten loop through
+//Layer One Colors Functionality
+//Set all squares to smaller size based on length of color word clicked
 function loopThrough() {
-const changePosition = () => this.classList.toggle('smaller');
-
-//*2 on off
-for(let i = 0; i < this.textContent.length * 2; i++) { setTimeout(changePosition, i * 700);
-
+  const changePosition = () => {
+    for(let y=0; y < colorBoxes.length; y++)
+      colorBoxes[y].classList.toggle('smaller');
+    }
+  for(let i = 0; i < this.textContent.length * 2; i++) {
+      setTimeout(changePosition, i * 700);
   };
 };
 
-//layer changes one to two
-
+//Remove Color layer to reveal second layer - first number set
 function switchLayerOne() {
   const switchOne = () => {
     layer1.classList.add('hide');
@@ -42,18 +43,21 @@ for (let i=0; i < colorBoxes.length; i++) {
 
 
 //layer 2 set number one loop
-
+//function turns gray the number of times of the number clicked
 function loopThroughSetOne() {
 
-  const changeGray = () => this.classList.toggle('gray');
+  const changeGray = () =>{
+    for(let j=0; j < numberOneBoxes.length; j++) {
+      numberOneBoxes[j].classList.toggle('gray');
+    }
+  }
 
   for(let i = 0; i < Number(this.textContent) * 2; i++) {
     setTimeout(changeGray, i * 800);
   };
 
 };
-//layer changes two to three
-
+//layer changes two number set one to layer three number set two
 function switchLayerTwo() {
   const switchTwo = () => {
     layer2.classList.add('hide');
@@ -70,6 +74,7 @@ for (let i = 0; i < numberOneBoxes.length; i++) {
 };
 
 //layer 3 reveal fortune
+//remove layer 3 number set two to reveal message
 
 function switchLayerThree() {
   const switchThree = () => {
@@ -84,14 +89,17 @@ function switchLayerThree() {
 for (let i = 0; i < numberTwoBoxes.length; i++) {
   numberTwoBoxes[i].addEventListener('click', switchLayerThree);
 };
+//layer 4
+//random message created after user selects number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 const randomMessage = ['You are amazing','Keep on shining like the star you are', 'You matter and deserve to take up space','Dream Big Little One','Big things are coming your way','You are the best you!','Never let anyone dull your sparkle','You can do anything you set your mind to AND MORE','You are a bright star in the world','Keep you head held high'];
-
 const revealMessage = () => {
-  randomMessage.sort((a, b) => {return 0.9 - Math.random()});
-  const randomMessageeText = document.createTextNode(randomMessage[0]);
-  const randomMessageParagraph = document.createElement('p');    randomMessageParagraph.appendChild(randomMessageText);
-  layerFour.appendChild(randomMessageParagraph);
+  const randomMessageText = randomMessage[getRandomInt(9)];
+  const layer4Paragraph = document.getElementById('layer4text')
+  layer4Paragraph.textContent = randomMessageText;
 };
 
-showFortune();
+revealMessage();
