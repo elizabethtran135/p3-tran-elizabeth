afec77fbfcda0dbc3eeab5de56b5e9cd54fef1b5
Elizabeth Tran <elizabethtran135@gmail.com>
2019-12-10 21:06:11 -0500

    Add layer 2 functions

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const colorBoxes = document.getElementsByClassName('colorblock');
 
 //layer Two
 const layerTwo = document.getElementById('layer2');
-const numberOneBoxes = document.getElementsByClassName('numberblock1');
+const numberOneBoxes = document.getElementsByClassName('numberblock');
 
 //layer Three
 const layerThree = document.getElementById('layer3');
@@ -17,27 +17,56 @@ const layerFour = document.getElementById('layer4');
 
 //functionality color and shorten loop through
 function loopThrough() {
-const changeGrayPosition = () => this.classList.toggle('smallergray');
+const changePosition = () => this.classList.toggle('smaller');
 
 //*2 on off
-for(let i = 0; i < this.textContent.length * 2; i++) { setTimeout(changeGrayPosition, i * 700);
+for(let i = 0; i < this.textContent.length * 2; i++) { setTimeout(changePosition, i * 700);
 
   };
 };
 
-
-//layer changes
+//layer changes one to two
 
 function switchLayerOne() {
   const switchOne = () => {
     layer1.classList.add('hide');
     layer2.classList.remove('hide');
   };
-  setTimeout(switchOne, this.textContent.length *2 * 700);
+  setTimeout(switchOne, this.textContent.length * 2 * 700);
 };
 //The Event Listener
 for (let i=0; i < colorBoxes.length; i++) {
   colorBoxes[i].addEventListener('click', loopThrough);
+  colorBoxes[i].addEventListener('click', switchLayerOne);
+};
+
+
+//layer 2 set number one loop
+
+function loopThroughSetOne() {
+
+  const changeGray = () => this.classList.toggle('gray');
+
+  for(let i = 0; i < Number(this.textContent) * 2; i++) {
+    setTimeout(changeGray, i * 800);
+  };
+
+};
+//layer changes two to three
+
+function switchLayerTwo() {
+  const switchTwo = () => {
+    layer2.classList.add('hide');
+    layer3.classList.remove('hide');
+  };
+  setTimeout(switchTwo, Number(this.textContent) * 2 * 1000);
+};
+
+//layer 2 event Listener
+
+for (let i = 0; i < numberOneBoxes.length; i++) {
+  numberOneBoxes[i].addEventListener('click', loopThroughSetOne);
+  numberOneBoxes[i].addEventListener('click', switchLayerTwo);
 };
 //function loopThrough() {
 //  const changeColor = () => this.classList.toggle('green');
